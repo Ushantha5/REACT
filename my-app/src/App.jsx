e2e5768f10de031f44d237dfee5893db8caf35c5
ushantha <ushanthamr@gmail.com>
2025-10-15 14:39:46 +0530

    sample try

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,13 +1,37 @@
 import "./App.css";
+function Header() {
+  return (
+    <>
+      <h1>Header</h1>
+    </>
+  );
+}
+
+function Welcome() {
+  return (
+    <>
+      <h2>Well come to UKI </h2>
+    </>
+  );
+}
+function Footer() {
+  return (
+    <>
+      <h1>Footer </h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <>
       <div>
-        <h1>React appliction</h1>
+        <h1>hello world</h1>
+        <Header />
+        <Footer />
+        <Welcome />
       </div>
     </>
   );
 }
-
 export default App;
